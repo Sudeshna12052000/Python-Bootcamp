2219f06362cba470eb052e068e42f2e0cdbe9e72
Sudeshna Bhushan <sudeshna.cs19@bmsce.ac.in>
2021-08-28 23:43:26 +0530

    Update Activity_06.py

--- Activity_06.py
+++ Activity_06.py
@@ -1,4 +1,6 @@
 my_list1 = input().split()
+my_list1[::-1]
+print("Reversed list = ",my_list1)
 new_list1 = my_list1[0:3]
 print("sliced list = ", new_list1)
 my_list1[0] = 0
@@ -7,4 +9,3 @@ my_list1[-1] = 0
 new_list1[-1] = 0
 print("replaced list-1 = ", my_list1)
 print("replaced list-2 = ",new_list1)
-
