2f1698afead755f8d8bf08117eb3532330191629
Sudeshna Bhushan <sudeshna.cs19@bmsce.ac.in>
2021-08-28 13:30:06 +0530

    Update Activity_02.py

--- Activity_02.py
+++ Activity_02.py
@@ -1,1 +1,7 @@
-
+Python 3.9.6 (v3.9.6:db3ff76da1, Jun 28 2021, 11:49:53) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+======= RESTART: /Users/sudeshnabhushan/Desktop/Python Bootcamp/hello.py =======
+Hello World!
+>>> 
