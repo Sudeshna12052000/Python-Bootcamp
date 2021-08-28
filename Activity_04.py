1660cfce47215e472d9c690cc25f754208b30be5
Sudeshna Bhushan <sudeshna.cs19@bmsce.ac.in>
2021-08-28 18:37:11 +0530

    Update Activity_04.py

--- Activity_04.py
+++ Activity_04.py
@@ -1,7 +1,11 @@
-a = int(input("Enter the first number to be added: "))
-b = int(input("Enter the second number to be added: "))
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
 c = a + b
-print("The sum of %d and %d is %d\n"%(a,b,c))
-print("", a, "+" , b , "=", c)
-print("The sum of " + repr(a) + ' and ' + repr(b) + ' is ' + repr(c))
-print (repr(a),'+',repr(b),'=',repr(c))
+print("The sum of %d + %d = %d\n"%(a, b, c))
+print(f'Sum of {a} + {b} is {c}')
+print (a, '+', b, '=',a+b)
+
+sum = a + b
+print("The sum of {0} and {1} is {2}".format(a, b, sum))
+
+print('The sum is %d' %(int(input('Enter first number: ')) + int(input('Enter second number: '))))
