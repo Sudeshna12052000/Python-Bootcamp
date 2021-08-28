dcaea7d997b58e8ff1da649b32e4aaf42e169de5
Sudeshna Bhushan <sudeshna.cs19@bmsce.ac.in>
2021-08-29 01:06:06 +0530

    Update Activity_07.py

--- Activity_07.py
+++ Activity_07.py
@@ -4,7 +4,6 @@ l=number.split()
 print(l[0],'+',l[1],'=',(int(l[0])+int(l[1])))
 
 
-number = ['2' , '3']
-c = number[0] + number[1]
-c = int(number[0]) + int(number[1])
-print(number[0] + "+" + number[1] + "=" + "5")
+no = [2,3,5]
+two,three,five=no
+print(str(two)+"+"+str(three)+"="+str(five))
