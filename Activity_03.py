0fe4306052b2bc453bc473864a72ce9de906e6c8
Sudeshna Bhushan <sudeshna.cs19@bmsce.ac.in>
2021-08-28 14:27:38 +0530

    Update Activity_03.py

--- Activity_03.py
+++ Activity_03.py
@@ -2,7 +2,5 @@ string1 = input("Enter the first string: ")
 string2 = input("Enter the second string:")
 concatenated_string = string1+string2
 print(concatenated_string)
-string3 = string1+" "+string2+"\n"
-print(string3)
 print(5 * concatenated_string)
-print(5 * string3)
+
