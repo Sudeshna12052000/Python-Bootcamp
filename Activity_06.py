59e353df673222624e3870bf459b5fb6582dc485
Sudeshna Bhushan <sudeshna.cs19@bmsce.ac.in>
2021-08-28 21:56:12 +0530

    Update Activity_06.py

---
+++ Activity_06.py
@@ -0,0 +1,10 @@
+my_list1 = input().split()
+new_list1 = my_list1[0:3]
+print("sliced list = ", new_list1)
+my_list1[0] = 0
+new_list1[0] = 0
+my_list1[-1] = 0
+new_list1[-1] = 0
+print("replaced list-1 = ", my_list1)
+print("replaced list-2 = ",new_list1)
+
