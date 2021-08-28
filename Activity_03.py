06243543adf19f6a3bbac851ceaa63f835ecc67b
Sudeshna Bhushan <sudeshna.cs19@bmsce.ac.in>
2021-08-28 13:47:10 +0530

    Update Activity_03.py

--- Activity_03.py
+++ Activity_03.py
@@ -1,19 +1,8 @@
->>> string1 = input("Enter the first string: ")
-Enter the first string: Good
->>> string2 = input("Enter the second string: ")
-Enter the second string: Morning
->>> concatenated_string = string1+string2
->>> print (concatenated_string)
-GoodMorning
->>> string3 = string1+" "+string2+"\n"
->>> print (string3)
-Good Morning
-
->>> print(3 * concatenated_string)
-GoodMorningGoodMorningGoodMorning
->>> print(3 * string3)
-Good Morning
-Good Morning
-Good Morning
-
->>> 
+string1 = input("Enter the first string: ")
+string2 = input("Enter the second string:")
+concatenated_string = string1+string2
+print(concatenated_string)
+string3 = string1+" "+string2+"\n"
+print(string3)
+print(5 * concatenated_string)
+print(5 * string3)
