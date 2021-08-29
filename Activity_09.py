ea368b5f295516ce23269f004b39f822fb2801d4
Sudeshna Bhushan <sudeshna.cs19@bmsce.ac.in>
2021-08-29 15:29:48 +0530

    Update Activity_09.py

--- Activity_09.py
+++ Activity_09.py
@@ -3,16 +3,6 @@ b=int(input("Breadth of the tromboloid"))
 h=int(input("Height of the tromboloid"))
 k=l**2+b**2+h**2
 v=float((b**2*h**2)/k**.5)
-print("volume of tromboloid",format(v,".3f"))
-r=(v*3/(4*3.14))**(1/3)
-print("radius",format(r,".3f"))
-
-
-l=int(input("length of the tromboloid"))
-b=int(input("Breadth of the tromboloid"))
-h=int(input("Height of the tromboloid"))
-k=l**2+b**2+h**2
-v=float((b**2*h**2)/k**.5)
-print("volume of tromboloid",v.format(".3f")
+print("volume of tromboloid",v.format(".3f"))
 r=((v*3/(4*3.14))**(1/3))
 print("radius",r.format(".3f")
