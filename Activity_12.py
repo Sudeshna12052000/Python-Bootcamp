c5110393c1d65a797cee4916f3e85f454bc39862
Sudeshna Bhushan <sudeshna.cs19@bmsce.ac.in>
2021-08-29 12:19:49 +0530

    Update Activity_12.py

---
+++ Activity_12.py
@@ -0,0 +1,12 @@
+def inp():
+    return input("Enter three numbers ")
+
+z=inp().split()
+
+
+p=int(z[0])
+q=int(z[1])
+r=int(z[2])
+
+a=max(p,q,r)
+print(a,"is the greatest number among ",p,",",q," and" ,r)
