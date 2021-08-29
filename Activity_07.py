60709d56b57c28c225bd45fb96d71c9241847e75
Sudeshna Bhushan <sudeshna.cs19@bmsce.ac.in>
2021-08-29 12:06:00 +0530

    Update Activity_07.py

--- Activity_07.py
+++ Activity_07.py
@@ -1,11 +1,5 @@
 number= input("Enter  two values: ")
 l=number.split()
-print(l[0],'+',l[1],'=',(int(l[0])+int(l[1])))
-
-
-
-number= input("Enter  two values: ")
-l=number.split()
 no = [l[0],l[1],(int(l[0])+int(l[1]))]
 two,three,five=no
 print(str(two)+"+"+str(three)+"="+str(five))
