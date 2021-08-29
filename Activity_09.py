2ec9a3779475f56dbbeba343a2c0ce7a09cd79f1
Sudeshna Bhushan <sudeshna.cs19@bmsce.ac.in>
2021-08-29 16:14:46 +0530

    Update Activity_09.py

--- Activity_09.py
+++ Activity_09.py
@@ -3,7 +3,7 @@ l=int(input("length of the tromboloid"))
 b=int(input("Breadth of the tromboloid"))
 h=int(input("Height of the tromboloid"))
 k=l**2+b**2+h**2
-v=float((b**2*h**2)/k**.5)
+v=float((b**2*h**2)/math.sqrt(k))
 print("volume of tromboloid",v.format(".3f"))
 r=((v*3/(4*math.pi))**(1/3))
 print("radius",r.format(".3f"))
